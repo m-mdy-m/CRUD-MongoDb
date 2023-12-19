c205a50a383b1a0205adb68926a2fd6b3aef58c7
mahdi <mahdimamashli1383@gmail.com>
2023-12-19 19:23:28 +0330

    load home rout

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const bodyParser = require('body-parser')
 const app = express()
 const path = require('path')
 const database = require('./database/database').connect
+
+// ROUTES 
+const homeROute = require("./routes/home")
 app.set('view engine', 'ejs')
 app.set('views','views')
 
 app.use(bodyParser.urlencoded({extended : false}))
 app.use(express.static(path.join(__dirname , "public")))
 
+
+app.use(homeROute)
 
 const start = async ()=>{
     try{
